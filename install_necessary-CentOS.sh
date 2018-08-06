8fb5b376c5f40a97b7c43647a100c2510a05baa9
Jason Hsu <duidae@gmail.com>
2018-08-06 21:00:33 +0800

    Update install_necessary-CentOS.sh

--- install_necessary-CentOS.sh
+++ install_necessary-CentOS.sh
@@ -6,8 +6,8 @@ sudo yum -y update
 echo "sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-echo "sudo yum -y install git make cmake unzip wget curl"
-sudo yum -y install git make cmake unzip wget curl
+echo "sudo yum -y install git make cmake unzip wget curl vim"
+sudo yum -y install git make cmake unzip wget curl vim
 
 echo "sudo yum -y install libreoffice kolourpaint grub-customizer"
 sudo yum -y install libreoffice kolourpaint grub-customizer
