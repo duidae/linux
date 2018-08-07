7b9d12ed0e02089334c31fbd644fdabc3c7dc01c
Jason Hsu <duidae@gmail.com>
2018-08-07 21:38:53 +0800

    Update install_necessary-CentOS.sh

--- install_necessary-CentOS.sh
+++ install_necessary-CentOS.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-echo "mkdir -p ~/vim-tmp"
+echo "Create vim temp directory..."
 mkdir -p ~/vim-tmp
 
-echo "sudo yum -y update"
+echo "Update yum..."
 sudo yum -y update
 
-echo "sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
+echo "Install epel, scl repository..."
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install centos-release-scl
 
-echo "sudo yum -y install git make cmake unzip wget curl vim"
+echo "Install git make cmake unzip wget curl vim..."
 sudo yum -y install unzip wget curl vim
 
-echo "sudo yum -y install libreoffice kolourpaint grub-customizer"
+echo "Install libreoffice kolourpaint grub-customizer..."
 sudo yum -y install libreoffice kolourpaint grub-customizer
